43cb1ec8aab1eb4c963110375715b192ef1cbff1
praveenjeykar <38877220+praveenjeykar@users.noreply.github.com>
2018-05-23 09:47:39 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 import webbrowser
-print("Content-type:text/html \n")
 
 
 class Movie():
+'''
+class Movie():
+        Attributes:
+            movie_title (str): displays the title of the movie.
+            poster_image (str): one of the look as image.
+            trailer_youtube (str): url of the youtube trailer of the movie.
+'''
+        # we have taken a function which accepts four given arguments.
         def __init__(self, movie_title, poster_image, trailer_youtube):
                 self.title = movie_title
                 self.poster_image_url = poster_image
@@ -11,4 +18,3 @@ class Movie():
 
         def show_trailer(self):
             webbrowser.open(self.trailer_youtube_url)
-
